e17ce8e74595bf2a77b60656fa6507932f79420c
guido.enrique <guido.enrique@avature.net>
2022-01-07 17:53:10 -0300

    quick commit

--- model.py
+++ model.py
@@ -1,7 +1,8 @@
 from random import random
 import random as rm
 
-api_key = ''
+api_key = '77130712b79444a97ed470100c610371'
+token = '31ffac51c1ecb8cd92f3d193ac7aed7b9deb5a6fc78e1794f440c24a563bd185'
 words = ['rands', 'w0rd', 'inheritance',
                 'another', 'simple', 'spacex',
                 'etc']
