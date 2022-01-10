059d96f08f7409800571cb11918a9bae6e6d93df
guidoenr <guidoenr4@gmail.com>
2022-01-10 14:59:05 -0300

    quick commit

---
+++ conf.py
@@ -0,0 +1,26 @@
+# please, complete with the data in : https://trello.com/app-key
+credentials = {
+    "private": {
+        "api_key": "",
+        "token": "",
+        "id_board": "",
+    }
+}
+
+# please, complete with the data in : https://trello.com/b/WFPZ8fIc/<BOARD_NAME>.json
+workspace = {
+    "lists": {
+        "id_doing": "",
+        "id_done": "", 
+        "id_todo": "",
+        
+    },
+    "labels": {
+        "id_bug": "",
+        "id_issue": "",
+        "id_maintenance": "",
+        "id_research": "",
+        "id_task": "",
+        "id_test": "",
+    }    
+}
